a657f40f3f946b3c638dce5c941cc8bf3d91051f
MuhammadJunaidAkhter100 <muhammadjunaidakhter100@gmail.com>
2026-01-29 14:28:43 +0500

    Update CORS middleware to allow all origins during development

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Techfy Demand API")
 # Allow local frontend during development
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:8080"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
